256a01506691d37f17e4994e261f7afdc23f48a2
Easton <easton873@homtail.com>
2022-02-26 20:38:03 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+open up /tossatime and then run the following two commands
+
+`npm install`
+
+`npm install react-router-dom --save`
+
+Then run the command:
+
+`npm start`
+
+
+To launch the server in EC2 first
+
+`mvn compile`
+
+and then
+
+`sudo mvn exec:java -Dexec.mainClass=Server -Dexec.args="80"`
--- tossatime/src/App.js
+++ tossatime/src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
 
     render() { 
 
+        this.getData();
+
         return (
             <React.Fragment>
                 <NavBar />
@@ -14,7 +16,68 @@ class App extends Component {
                 <AllTimes />
             </React.Fragment>
         );
+
+        
     };
+
+    getData() {
+        // create a new XMLHttpRequest
+        var xhr = new XMLHttpRequest();
+        // get a callback when the server responds
+        xhr.addEventListener('load', () => {
+          // update the state of the component with the result here
+          console.log(xhr.responseText)
+        })
+
+        xhr.open('POST', 'http://44.202.141.234:80/createEvent');
+        let req = {
+            creator: "Jimmy",
+            description: "description is good",
+            name: "This is a cool event",
+            times: [
+                {
+                    year: "2022",
+                    month: "2",
+                    day: "26",
+                    hour: "6"
+                },
+                {
+                    year: "2022",
+                    month: "2",
+                    day: "26",
+                    hour: "7"
+                }
+            ]
+        }
+        
+        // xhr.open('POST', 'http://localhost:8080/voteEvent');
+        // let req = {
+        //     eventID: "982e375d-1ab9-4bbf-b6d9-9d1d3864eba4",
+        //     name: "This is a cool event",
+        //     times: [
+        //         {
+        //             year: "2022",
+        //             month: "2",
+        //             day: "26",
+        //             hour: "6"
+        //         },
+        //         {
+        //             year: "2022",
+        //             month: "2",
+        //             day: "26",
+        //             hour: "7"
+        //         }
+        //     ]
+        // }
+        
+        // xhr.open('POST', 'http://localhost:8080/getEvent');
+        // let req = {
+        //     eventID: "982e375d-1ab9-4bbf-b6d9-9d1d3864eba4"
+        // }
+
+        xhr.send(JSON.stringify(req));
+      }
+
 };
 
 export default App;
